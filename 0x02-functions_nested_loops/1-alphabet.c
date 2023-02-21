ecec14228cb60739aaa7b4f72fc863c0dc6f7687
Mpume-Lelo <nnn.mdletshe@gmail.com>
2023-02-21 13:05:18 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints lowercase alphabets
+ */
+
+void print_alphabet(void)
+{
+	char la;
+
+	for (la = 'a'; la <= 'z';; la++)
+		_putchar(la);
+
+	putchar('\n');
+
+}
