44473f7ed43a0401eb7293a83935ab99101a6e8a
Mpume-Lelo <nnn.mdletshe@gmail.com>
2023-02-21 14:59:56 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,19 +2,18 @@
 
 /**
  * main - Prints _putchar and a new line
- * Return: Always 0
+ * Return: Always 0 (success)
  */
-
 int main(void)
 {
-		int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
-	int count, sz;
-
-	sz = sizeof(str) / sizeof(int);
-	for (count = 0; count < sz; count++)
-	{
-		_putchar(str[count]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
