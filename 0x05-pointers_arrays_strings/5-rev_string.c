6daf3b9cade84e9b70658c74e7299844c165472c
Mpume-Lelo <nnn.mdletshe@gmail.com>
2023-02-27 17:01:39 -0800

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,7 +8,7 @@
 
 void rev_string(char *s)
 {
-	char rev = s[0];
+	char rvs = s[0];
 	int counter = 0;
 	int i;
 
@@ -17,8 +17,8 @@ void rev_string(char *s)
 	for (i = 0; i < counter; i++)
 	{
 		counter--;
-		rev = s[i];
+		rvs = s[i];
 		s[i] = s[counter];
-		s[counter] = rev;
+		s[counter] = rvs;
 	}
 }
