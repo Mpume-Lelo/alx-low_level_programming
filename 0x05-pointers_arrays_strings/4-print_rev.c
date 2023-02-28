68a619695c656272a928ed844012072f251f02a5
Mpume-Lelo <nnn.mdletshe@gmail.com>
2023-02-27 16:39:13 -0800

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse, followed by a new line
+ * @s: string to be printed
+ * return: 0
+ */
+void print_rev(char *s)
+{
+	int n = 0;
+
+	while (s[n] != '\0')
+		n++;
+	for (n = n - 1; n > 0; n--)
+		_putchar(s[n]);
+
+	_putchar('\n');
+}
