8a7962a06b7ff7f79101c1a5a8eb8682f2bb46c8
Mpume-Lelo <nnn.mdletshe@gmail.com>
2023-02-27 17:42:21 -0800

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,9 +12,9 @@ void print_array(int *a, int n)
 
 	for (r = 0; r < (n - 1); r++)
 	{
-		printf(" % d", a[r]);
+		printf("%d", a[r]);
 		if (r != n - 1)
-			printf(", ");
+			printf(",");
 	}
 			printf("\n");
 }
