c0c31c6881f157fcb937dce2afec0166384b0764
Mpume-Lelo <nnn.mdletshe@gmail.com>
2023-03-02 01:48:05 -0800

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * leet - encodes string into 1337
+ * @n: input value
+ * Return: n value
+ */
+char *leet(char *n)
+{
+	int i, h;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		for (h = 0; h < 10; h++)
+		{
+			if (n[i] == s1[h])
+			{
+				n[i] = s2[h];
+			}
+		}
+	}
+	return (n);
+}
